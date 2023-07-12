b5873e2faaf9423e0cc6e2879ee45217b6cc94d6
harutiro <harutiro2027@outlook.jp>
2023-07-12 12:40:49 +0900

    add: countアプリを作成した。

--- lib/main.dart
+++ lib/main.dart
@@ -2,26 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:count_app/banana_counter.dart';
 
 void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
 
   final controller = TextEditingController();
 
-  hoge(){
-    print(controller.text);
-  }
+  var countNumber = 0;
 
-  final textFiled = TextField(
-    controller: controller,
-    decoration: const InputDecoration(
-      border: OutlineInputBorder(),
-      labelText: "名前",
-      hintText: "名前を入力してください",
-    ),
-  );
+  @override
+  Widget build(BuildContext context) {
+    hoge(){
+      // print(controller.text);
+      setState(() {
+        countNumber++;
+      });
+      debugPrint(countNumber.toString());
+    }
+
+    final textFiled = TextField(
+      controller: controller,
+      decoration: const InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: "名前",
+        hintText: "名前を入力してください",
+      ),
+    );
 
 
 
 
-  final button = ElevatedButton(
+    final button = ElevatedButton(
       onPressed: (){
         hoge();
       },
@@ -29,44 +46,48 @@ void main() {
         shape: const StadiumBorder(),
       ),
       child: const Text("ボタン"),
-  );
+    );
 
 
 
-  final col = Column(
+    final col = Column(
 
-    mainAxisAlignment: MainAxisAlignment.center,
-    crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
 
-    children: [
-      // Image.asset("assets/images/penguin.jpg"),
-      // Image.network("https://flutter-image-network.web.app/inu.jpeg"),
+        children: [
+          // Image.asset("assets/images/penguin.jpg"),
+          // Image.network("https://flutter-image-network.web.app/inu.jpeg"),
 
-      // BananaCounter(
-      //   number: 888,
-      // )
+          BananaCounter(
+            number: countNumber,
+          ),
 
-      button,
-      textFiled
-    ]
-  );
+          button,
+          // textFiled
+        ]
+    );
 
-  final con = Container(
-    color: Colors.orange,
-    width: 600,
-    height: 600,
-    padding: const EdgeInsets.all(10),
-    child: col,
-  );
+    final con = Container(
+      color: Colors.orange,
+      width: 600,
+      height: 600,
+      padding: const EdgeInsets.all(10),
+      child: col,
+    );
 
 
-  final a = MaterialApp(
-    home: Scaffold(
-      body:Center(
-        child: con,
-      )
-    )
-  );
+    final a = MaterialApp(
+        home: Scaffold(
+            body:Center(
+              child: con,
+            )
+        )
+    );
 
-  runApp(a);
+    return a;
+  }
+
 }
+
+
