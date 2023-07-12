695585344df25c34c6bba87a52b1b4f3653fe4fb
harutiro <harutiro2027@outlook.jp>
2023-07-12 09:36:08 +0900

    update: 写真の追加

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:flutter/material.dart';
 
 void main() {
 
-  const col = Column(
+  final col = Column(
 
     mainAxisAlignment: MainAxisAlignment.center,
     crossAxisAlignment: CrossAxisAlignment.center,
 
     children: [
-      Text("レモン"),
-      Text("みかん"),
-      Text("りんご"),
-  ]);
+      Image.asset("assets/images/penguin.jpg"),
+      Image.network("https://flutter-image-network.web.app/inu.jpeg"),
+    ]
+  );
 
 
-  const a = MaterialApp(
+  final a = MaterialApp(
     home: Scaffold(
       body:Center(
         child: col,
